c95a42552a45a260357be0d424aaaa876c9071fc
m.axmadjonov <m.axmadjonov@newuu.uz>
2025-01-17 12:25:33 +0500

    My first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int grade;
+    cout << " Hello, what is you grade?" << endl;
+    cin >> grade;
+    cout << " So, your grade is: " << grade << endl;
+
+
+return 0;
+}
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
